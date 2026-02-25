2ce2cbee0e25a9260b767cf4a07624457bd59e4e
Ayham Ibrahim <ayhamfacebook1999@gmail.com>
2026-02-26 02:07:31 +0300

    fix geolocation

--- config/hypermsg.php
+++ config/hypermsg.php
@@ -4,4 +4,5 @@ return [
     'base_url' => env('WHATSAPP_BASE_URL', 'https://message.dashboard.technoplus.tech/whatsapp/api/v1'),
     'send_text_path' => env('WHATSAPP_SEND_TEXT_PATH', 'message/text/send'),
     'session_id' => env('WHATSAPP_SESSION_ID'),
+    'access_token' => env('WHATSAPP_ACCESS_TOKEN'),
 ];
